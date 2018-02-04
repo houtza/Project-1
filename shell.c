6a0226c972825ce54d913a6c81737df4cded5d86
houtza <houtza@uni.edu>
2018-02-04 13:34:25 -0600

    Fixed the code indentation

--- shell.c
+++ shell.c
@@ -22,55 +22,56 @@ void printInColor(char *stringToColor); //Takes in the address of a string and p
 
 int main(int argc, char *argv[])
 {
+	
   
-  char *myArgv[ARGV_SIZE];  // an array of pointers to strings, possibly move inside while loop so that array is cleared each time
-  char *token;
-  pid_t pid;
+	char *myArgv[ARGV_SIZE];  // an array of pointers to strings, possibly move inside while loop so that array is cleared each time
+	char *token;
+	pid_t pid;
 
-  if ((pid = fork()) == 0) { //Creats a parent and child fork of the code
-    int i;
-    while(1){              //Loops continuously untill "quit" is entered
-      char commands[80];//Can handle a imput up to 80 caricters
-      int tokenCounter=0;   //The counter for the # of tokens
+	if ((pid = fork()) == 0) { //Creats a parent and child fork of the code
+		int i;
+		while(1){              //Loops continuously untill "quit" is entered
+			char commands[80];//Can handle a imput up to 80 caricters
+			int tokenCounter=0;   //The counter for the # of tokens
 
-      for(i=0;i<10;i++){   //clears the array each iteration of the while loop
-	myArgv[i]=NULL;
-      }
+			for(i=0;i<10;i++){   //clears the array each iteration of the while loop
+				myArgv[i]=NULL;
+			}
      
-      printWorkingDir();   
-      scanf("%[^\n]%*c",commands);    
+			printWorkingDir();   
+			scanf("%[^\n]%*c",commands);    
 
 
-      token = strtok(commands," ");   //Get the first token from the sentence to see if it is "exit"
-      if(!(strcmp(token,"exit"))){  
-	//printf("%sExit\n");
-	return 0;
-      }
-      tokenCounter++;
+			token = strtok(commands," ");   //Get the first token from the sentence to see if it is "exit"
+			if(!(strcmp(token,"exit"))){  
+				//printf("%sExit\n");
+				return 0;
+			}
+			tokenCounter++;
  
-      myArgv[tokenCounter]=token; 
-      printf("%s\n",myArgv[tokenCounter]);// Tester for printing out the tokens saved in the string pointer array
+			myArgv[tokenCounter]=token; 
+			printf("%s\n",myArgv[tokenCounter]);// Tester for printing out the tokens saved in the string pointer array
 
 
-      while(token!=NULL){    //Loop throught the remaining tokens
+			while(token!=NULL){    //Loop throught the remaining tokens
      
-	token =strtok(NULL," ");
+				token =strtok(NULL," ");
 
-	if(token!=NULL){
-	  tokenCounter++;      
-	  myArgv[tokenCounter]=token;
-	  printf("%s\n",myArgv[tokenCounter]); // Tester for printing out the tokens saved in the string pointer array    
+				if(token!=NULL){
+					tokenCounter++;      
+					myArgv[tokenCounter]=token;
+					printf("%s\n",myArgv[tokenCounter]); // Tester for printing out the tokens saved in the string pointer array    
 
-	}
-      }
+				}
+			}
       
-       if(execvp(*myArgv,myArgv)<0);{  //Still trying to get execvp to work correctly
-	  return 0;   
-       }
-    }
-  }else{
-    waitpid(-1,NULL,0);//Parrent process waits for the child process to end
-  }
+			if(execvp(*myArgv,myArgv)<0);{  //Still trying to get execvp to work correctly
+				return 0;   
+			}
+		}
+	}else{
+		waitpid(-1,NULL,0);//Parrent process waits for the child process to end
+	}
 
  return 0;
 }
@@ -78,13 +79,14 @@ int main(int argc, char *argv[])
 
 //Takes in the address of a string and prints it in red
 void printInColor(char *stringToColor){
-  printf(RED  "%s$ "RESET, stringToColor);
+	
+	printf(RED  "%s$ "RESET, stringToColor);
 }
 
 //Prints the current working directory path
 void printWorkingDir(){
-  char cwd[1024];
-  if (getcwd(cwd, sizeof(cwd)) != NULL){  //Gets current working directory
-    printInColor(cwd);
-  }
+	char cwd[1024];
+	if (getcwd(cwd, sizeof(cwd)) != NULL){  //Gets current working directory
+		printInColor(cwd);
+	}
 }
